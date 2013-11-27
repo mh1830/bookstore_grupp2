04227204b1dab9185834c0191f53bf4a52ada765
Martin Hansson <sylle90@hotmail.com>
2013-11-27 16:50:14 +0100

    Added a start function that makes the javascript push the inputfields to the html page after the DOM has loaded.

--- script/js.js
+++ script/js.js
@@ -1,9 +1,13 @@
 //added a loop that places the different input slots that we need on the page. CSS to come later
 //may need to be modified later.
+function start (){
+	var titles = ["ISBN", "Titel", "Författare", "F-Pris", "Leveransdatum", "Utpris", "Hyllnummer", "Antal ex"];
 
-var titles = ["ISBN", "Titel", "Författare", "F-Pris", "Leveransdatum", "Utpris", "Hyllnummer", "Antal ex"];
+	for (var i=0;i<titles.length;i++)
+	{
+	$('#mainSection').append('<div class = "inputFields">' + titles[i] + "<br>" + '<input class="lagersaldoSida" type="text">' + "<br>" + "</div>");
+	};
+};
 
-for (var i=0;i<titles.length;i++)
-{
-$('#mainSection').append('<div class = "inputFields">' + titles[i] + "<br>" + '<input class="lagersaldoSida" type="text">' + "<br>" + "</div>");
-}
+$(start);
+
