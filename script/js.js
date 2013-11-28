ae285d012ba7f26680a1c649ff1d82f6221af762
mh1830 <sylle90@hotmail.com>
2013-11-28 11:18:42 +0100

    Made the inputfields work
    Created a javascript/json command that allows us to add new titles into
    the database, still needs some correcting but now it works.

--- script/js.js
+++ script/js.js
@@ -2,12 +2,46 @@
 //may need to be modified later.
 function start (){
 	var titles = ["Antal ex", "Hyllnummer", "Utpris", "Leveransdatum", "F-Pris", "Författare","Titel" , "ISBN"];
+	var namn = ["antal", "hylla", "utpris", "leveransdatum", "fpris", "forfattare","titel","isbn"];
 
 	for (var i=0;i<titles.length;i++)
 	{
-	$('#mainSection').prepend('<div class = "inputFields">' + titles[i] + "<br>" + '<input class="lagersaldoSida" type="text">' + "<br>" + "</div>");
+	$('#mainSection').prepend('<div class = "inputFields">' + titles[i] + "<br>" + '<input class="lagersaldoSida" type="text" id="' + 
+		namn[i] + '">' + "<br>" + "</div>");
 	};
+	collectFormData();
 };
+
+//a function to collent the data from the input fields
+function collectFormData () {
+	$('#mainSection').submit (function(){
+		var data = {};
+		//prevents the page from reloading when you hit submit.
+		event.preventDefault();
+		//looping through the input fields to collect the data from the inputfields.
+		$('#mainSection .inputFields input').each(function(){
+			data [this.id] = this.value
+		}); 
+		//sending the data to the function sendFormData
+		sendFormData(data);
+	}); 
+};
+//
+function sendFormData (data) {
+
+	data.action = "insertNewBook"; console.log(data)
+	$.ajax({
+		url:"sql",
+		cache:false,
+		data: data,
+		success:function(x){
+			console.log(x);
+		},
+		error:function(x){
+			console.log(x.responseJSON);
+		}
+	});
+}
 
 // Calculator for sales price of the book. Includes both inkMoms and exMoms.
 function utpris(fpris){
